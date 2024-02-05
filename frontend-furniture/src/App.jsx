6d5584e7ba3295d4bceb080baecb7f3d4d4e8341
SBEGENJOV <seydi19981995@gmail.com>
2024-02-06 00:11:58 +0300

    add/ global notification

--- frontend-furniture/src/App.jsx
+++ frontend-furniture/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <>deneme</>;
+  return (
+    <header>
+      <div className="global-notification">
+        <div className="container">
+          <p>
+            Mobilya Denilince Aklına ilk geldiysek
+            <a href="shop.html"> Şimdi AL</a>
+          </p>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default App;
