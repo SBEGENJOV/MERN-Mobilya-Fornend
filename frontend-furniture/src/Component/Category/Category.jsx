d97737af89ebc3ad857216842ca2295eabdfb7de
SBEGENJOV <seydi19981995@gmail.com>
2024-02-08 23:02:13 +0300

    add/category img

--- frontend-furniture/src/Component/Category/Category.jsx
+++ frontend-furniture/src/Component/Category/Category.jsx
@@ -1,11 +1,14 @@
 import "./Category.css";
 import PropTypes from "prop-types";
-//import "./Header";
 
 export default function Category() {
   return (
     <header>
-      <div className="header-center" id="sidebar">
+      <div
+        className="header-center"
+        id="sidebar"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
         <nav className="navigation">
           <ul className="menu-list">
             <li className="menu-list-item megamenu-wrapper">
@@ -17,91 +20,9 @@ export default function Category() {
                 <div className="menu-dropdown-megamenu">
                   <div className="megamenu-links">
                     <div className="megamenu-products">
-                      <h3 className="megamenu-products-title">Shop Style</h3>
-                      <ul className="megamenu-menu-list">
-                        <li>
-                          <a href="#">Shop Standard</a>
-                        </li>
-                        <li>
-                          <a href="#">Shop Full</a>
-                        </li>
-                        <li>
-                          <a href="#">Shop Only Categories</a>
-                        </li>
-                        <li>
-                          <a href="#">Shop Image Categories</a>
-                        </li>
-                        <li>
-                          <a href="#">Shop Sub Categories</a>
-                        </li>
-                        <li>
-                          <a href="#">Shop List</a>
-                        </li>
-                        <li>
-                          <a href="#">Hover Style 1</a>
-                        </li>
-                        <li>
-                          <a href="#">Hover Style 2</a>
-                        </li>
-                        <li>
-                          <a href="#">Hover Style 3</a>
-                        </li>
-                      </ul>
+                      <h3 className="megamenu-products-title">Koltuk</h3>
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/220px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"></img>
                     </div>
-                    <div className="megamenu-products">
-                      <h3 className="megamenu-products-title">Filter Layout</h3>
-                      <ul className="megamenu-menu-list">
-                        <li>
-                          <a href="#">Sidebar</a>
-                        </li>
-                        <li>
-                          <a href="#">Filter Side Out</a>
-                        </li>
-                        <li>
-                          <a href="#">Filter Dropdown</a>
-                        </li>
-                        <li>
-                          <a href="#">Filter Drawer</a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="megamenu-products">
-                      <h3 className="megamenu-products-title">Shop Loader</h3>
-                      <ul className="megamenu-menu-list">
-                        <li>
-                          <a href="#">Shop Pagination</a>
-                        </li>
-                        <li>
-                          <a href="#">Shop Infinity</a>
-                        </li>
-                        <li>
-                          <a href="#">Shop Load More</a>
-                        </li>
-                        <li>
-                          <a href="#">Cart Modal</a>
-                        </li>
-                        <li>
-                          <a href="#">Cart Drawer</a>
-                        </li>
-                        <li>
-                          <a href="#">Cart Page</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="megamenu-single">
-                    <a href="#">
-                      <img src="img/mega-menu.jpg" alt="" />
-                    </a>
-                    <h3 className="megamenu-single-title">
-                      JOIN THE LAYERING GANG
-                    </h3>
-                    <h4 className="megamenu-single-subtitle">
-                      Suspendisse faucibus nunc et pellentesque
-                    </h4>
-                    <a href="#" className="megamenu-single-button btn btn-sm">
-                      Shop Now
-                    </a>
                   </div>
                 </div>
               </div>
