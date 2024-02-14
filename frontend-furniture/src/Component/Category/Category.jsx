a53bb0500d8c4c4ba301b88754efc63371a876d8
SBEGENJOV <seydi19981995@gmail.com>
2024-02-14 21:54:33 +0300

    add/all category header

--- frontend-furniture/src/Component/Category/Category.jsx
+++ frontend-furniture/src/Component/Category/Category.jsx
@@ -1,7 +1,16 @@
 import "./Category.css";
 import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { categoryViewAction } from "../../Redux/Slices/categories/categoriesSlice";
+import { useEffect } from "react";
 
 export default function Category() {
+  const dispatch = useDispatch();
+  const { categories } = useSelector((state) => state?.category);
+  //dispatch
+  useEffect(() => {
+    dispatch(categoryViewAction());
+  }, [dispatch]);
   return (
     <header>
       <div
@@ -11,34 +20,36 @@ export default function Category() {
       >
         <nav className="navigation">
           <ul className="menu-list">
-            <li className="menu-list-item megamenu-wrapper">
-              <a href="shop.html" className="menu-link">
-                Shop
-                <i className="bi bi-chevron-down"></i>
-              </a>
-              <div className="menu-dropdown-wrapper">
-                <div className="menu-dropdown-megamenu">
-                  <div className="megamenu-links">
-                    <div className="megamenu-products">
-                      <h3>Koltuk</h3>
-                      <img src="https://e-commerce-udemy.netlify.app/img/categories/categories1.png"></img>
-                    </div>
-                    <div className="megamenu-products">
-                      <h3>Deneme</h3>
-                      <img src="https://e-commerce-udemy.netlify.app/img/categories/categories2.png"></img>
-                    </div>
-                    <div className="megamenu-products">
-                      <h3>Deneme 2</h3>
-                      <img src="https://e-commerce-udemy.netlify.app/img/categories/categories4.png"></img>
-                    </div>
-                    <div className="megamenu-products">
-                      <h3>Deneme 3</h3>
-                      <img src="https://e-commerce-udemy.netlify.app/img/categories/categories5.png"></img>
+            {categories?.categories?.map((category) => (
+              <li className="menu-list-item megamenu-wrapper">
+                <a href="shop.html" className="menu-link">
+                  {category.name}
+                  <i className="bi bi-chevron-down"></i>
+                </a>
+                <div className="menu-dropdown-wrapper">
+                  <div className="menu-dropdown-megamenu">
+                    <div className="megamenu-links">
+                      <div className="megamenu-products">
+                        <h3>Koltuk</h3>
+                        <img src="https://e-commerce-udemy.netlify.app/img/categories/categories1.png"></img>
+                      </div>
+                      <div className="megamenu-products">
+                        <h3>Deneme</h3>
+                        <img src="https://e-commerce-udemy.netlify.app/img/categories/categories2.png"></img>
+                      </div>
+                      <div className="megamenu-products">
+                        <h3>Deneme 2</h3>
+                        <img src="https://e-commerce-udemy.netlify.app/img/categories/categories4.png"></img>
+                      </div>
+                      <div className="megamenu-products">
+                        <h3>Deneme 3</h3>
+                        <img src="https://e-commerce-udemy.netlify.app/img/categories/categories5.png"></img>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
@@ -47,6 +58,5 @@ export default function Category() {
 }
 
 Category.propTypes = {
-  isSearchShow: PropTypes.bool,
-  setIsSearchShow: PropTypes.func,
+  categoryItem: PropTypes.object,
 };
--- frontend-furniture/src/Component/Header/Header.jsx
+++ frontend-furniture/src/Component/Header/Header.jsx
@@ -66,7 +66,7 @@ export default function Header() {
             </div>
           </div>
         </div>
-      </div>{" "}
+      </div>
       <Category />
     </header>
   );
