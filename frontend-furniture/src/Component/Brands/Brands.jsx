ba6b61a21a8e3e8b9372fc1a9c27b04b4b89eee2
SBEGENJOV <seydi19981995@gmail.com>
2024-02-20 22:39:00 +0300

    add/brands component

---
+++ frontend-furniture/src/Component/Brands/Brands.jsx
@@ -0,0 +1,63 @@
+//import BrandItem from "./BrandItem";
+import "./Brands.css";
+
+const Brands = () => {
+  return (
+    <section className="brands">
+      <div className="container">
+        <ul className="brand-list">
+          <li className="brand-item">
+            <a href="#">
+              <img
+                src="https://cevrebilimkoleji.com/upload/logo-new.png"
+                alt=""
+              />
+            </a>
+          </li>
+          <li className="brand-item">
+            <a href="#">
+              <img
+                src="https://cevrebilimkoleji.com/upload/logo-new.png"
+                alt=""
+              />
+            </a>
+          </li>
+          <li className="brand-item">
+            <a href="#">
+              <img
+                src="https://cevrebilimkoleji.com/upload/logo-new.png"
+                alt=""
+              />
+            </a>
+          </li>
+          <li className="brand-item">
+            <a href="#">
+              <img
+                src="https://cevrebilimkoleji.com/upload/logo-new.png"
+                alt=""
+              />
+            </a>
+          </li>
+          <li className="brand-item">
+            <a href="#">
+              <img
+                src="https://cevrebilimkoleji.com/upload/logo-new.png"
+                alt=""
+              />
+            </a>
+          </li>
+          <li className="brand-item">
+            <a href="#">
+              <img
+                src="https://cevrebilimkoleji.com/upload/logo-new.png"
+                alt=""
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Brands;
--- frontend-furniture/src/Pages/HomePage.jsx
+++ frontend-furniture/src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Product from "../Component/Product/Product";
 import Sliders from "../Component/Slider/Slider";
 import Campaigns from "../Component/Campaigns/Campaigns";
 import Blogs from "../Component/Blog/Blog";
+import Brands from "../Component/Brands/Brands";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Product />
       <Campaigns />
       <Blogs />
+      <Brands />
     </React.Fragment>
   );
 };
